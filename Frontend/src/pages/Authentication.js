2654b01009b1c9e89d15c6dd748738b9ba6d4810
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2023-05-10 00:26:55 +0530

    optimized

--- Frontend/src/pages/Authentication.js
+++ Frontend/src/pages/Authentication.js
@@ -55,7 +55,7 @@ const Authentication = () => {
       navigate("/quiztools/create-quiz")
     }
     else if (apiData.status === 500) {
-      isLoading(false)
+    setLoader(false)
       setInvalidCredentials(true)
     }
   }
