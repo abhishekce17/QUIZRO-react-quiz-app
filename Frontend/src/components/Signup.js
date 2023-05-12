527b06ab81ac1ad62995f79d69c68b5dfe74216b
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2023-05-12 23:28:04 +0530

    adding error msg on sign up

--- Frontend/src/components/Signup.js
+++ Frontend/src/components/Signup.js
@@ -12,6 +12,7 @@ const Signup = () => {
     email: "",
     password: ""
   })
+  const [errorMsg, setErrorMsg] = useState({ status: false, Msg: "" })
   const [check, setCheck] = useState({ check: "", status: true })
   function handleChange(e) {
     const { name, value } = e.target
@@ -24,7 +25,7 @@ const Signup = () => {
     if (check.check == signCredential.password) handleEvent()
     setCheck({ ...check, status: check.check == signCredential.password })
   }
-  
+
   async function handleEvent() {
     setLoader(true);
     const apiData = await fetch('https://quizro-quiz-backend.vercel.app/api/auth/signup', {
@@ -39,8 +40,11 @@ const Signup = () => {
       localStorage.setItem("token", response.authToken)
       navigate("/quiztools/create-quiz")
     }
-    else{
-      console.log(response)
+    else {
+      setErrorMsg(() => {
+        return { status: true, Msg: response.errors[0] }
+      })
+      setLoader(false);
     }
 
   }
@@ -48,29 +52,30 @@ const Signup = () => {
 
   return (
     <>
-    {
-      isLoading ? <Loading /> :
-      <form method="POST" className="form" onSubmit={formEvent} >
-        <div>
-          {!check.status && <div className="alert" > password doesn't match </div>}
-          <div className="emailSignup">
-            <div className="name-section" >
-              <input type="text" name="first name" value={signCredential["first name"]} placeholder="First name" onChange={handleChange} required />
-              <input type="text" name="last name" value={signCredential["last name"]} placeholder="Last name" onChange={handleChange} required />
+      {
+        isLoading ? <Loading /> :
+          <form method="POST" className="form" onSubmit={formEvent} >
+            <div>
+              {!check.status && <div className="alert" > password doesn't match </div>}
+              {errorMsg.status && <div className="alert" > {errorMsg.Msg} </div>}
+              <div className="emailSignup">
+                <div className="name-section" >
+                  <input type="text" name="first name" value={signCredential["first name"]} placeholder="First name" onChange={handleChange} required />
+                  <input type="text" name="last name" value={signCredential["last name"]} placeholder="Last name" onChange={handleChange} required />
+                </div>
+                <input type="email" name="email" value={signCredential.email} placeholder="email" onChange={handleChange} required />
+                <div className="password-section">
+                  <input type="password" name="check" value={check.check} placeholder="password" onChange={handleChange} required />
+                  <input type="password" name="password" value={signCredential.password} placeholder="confirm password" onChange={handleChange} required />
+                </div>
+              </div>
+              <button className="signup" type="submit"  >Get Started</button>
+              <center>
+                <p>Already have account, <Link to="/login">login here</Link> </p>
+              </center>
             </div>
-            <input type="email" name="email" value={signCredential.email} placeholder="email" onChange={handleChange} required />
-            <div className="password-section">
-              <input type="password" name="check" value={check.check} placeholder="password" onChange={handleChange} required />
-              <input type="password" name="password" value={signCredential.password} placeholder="confirm password" onChange={handleChange} required />
-            </div>
-          </div>
-          <button className="signup" type="submit"  >Get Started</button>
-          <center>
-            <p>Already have account, <Link to="/login">login here</Link> </p>
-          </center>
-        </div>
-      </form>
-    }
+          </form>
+      }
     </>
   )
 }
