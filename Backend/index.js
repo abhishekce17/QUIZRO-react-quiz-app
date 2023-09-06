16f5788249602d78997582b6529e57908403e453
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2023-09-06 15:18:54 +0530

    Revert "#"
    This reverts commit be6c57951762a7c64998d67423e5ee983d302af2.

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-//jshint esversion:6
+	//jshint esversion:6
 const connectToMongo = require("./db")
 const express = require("express");
 const cors = require("cors")
@@ -9,12 +9,12 @@ const passport = require("passport");
 const app = express();
 connectToMongo();
 
-app.use(session(
-  {
+app.use(	session(
+    {
     name: "session",
     keys: ["clientsSecretQuizroAuthentication"],
     maxAge: 24 * 60 * 60 * 100,
-  }))
+}))
 
 app.use(
   cors({
@@ -28,7 +28,15 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(__dirname + "/public"));
+
+// app.use(
+// 	cors({
+// 		origin: "*",
+// 		methods: "GET,POST,PUT,DELETE",
+// 		credentials: true,
+// 	})
+// );
+app.use(express.static( __dirname+"/public"));
 app.set("view engine", "ejs");
 app.use(express.json())
 // app.use(bodyParser.urlencoded({extended:true}));
@@ -37,8 +45,8 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/quizoperations", require("./routes/quizoperations"));
 app.use("/api/submit/responses", require("./routes/submitresponse"));
 
-app.listen(5000, function (err) {
-  if (!err) {
-    console.log("server is running on port 5000");
-  }
+app.listen(5000, function(err){
+    if(!err){
+        console.log("server is running on port 5000");
+    }
 })
