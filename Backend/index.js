e7337a17dea9f770128bd98f0ebdc3130c0f6914
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2023-09-06 16:42:21 +0530

    optimized

--- Backend/index.js
+++ Backend/index.js
@@ -2,45 +2,33 @@
 const connectToMongo = require("./db")
 const express = require("express");
 const cors = require("cors")
-var session = require('cookie-session');
+var session = require('express-session');
 const passport = require("passport");
 
 
 const app = express();
 connectToMongo();
 
-
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://quizro-quiz.vercel.app");
-  res.header("Access-Control-Allow-Credentials", true);
-
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
 app.use(session(
   {
     name: "session",
     keys: ["clientsSecretQuizroAuthentication"],
     maxAge: 24 * 60 * 60 * 100,
+    cookie: {
+      sameSite: "none",
+    }
   }))
 
-// app.use(
-//   cors({
-//     origin: "https://quizro-quiz.vercel.app",
-//     methods: "GET,POST,PUT,DELETE",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://quizro-quiz.vercel.app",
+    preflightContinue: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 
