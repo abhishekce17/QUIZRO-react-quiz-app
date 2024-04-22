eed6a45f600011b2af34c59c77473b63de3df9e7
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2024-04-22 22:49:49 +0530

    cors middleware position changed

--- Backend/index.js
+++ Backend/index.js
@@ -6,14 +6,13 @@ const bodyParser = require("body-parser")
 
 
 const app = express();
-connectToMongo();
-
 app.use(cors())
 
+connectToMongo();
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/quizoperations", require("./routes/quizoperations"));
