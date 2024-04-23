81bbd76df7e8b8aece091110458fed3b7e136a3d
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2024-04-23 23:22:23 +0530

    auth key changed

--- Backend/middleware/loginstatus.js
+++ Backend/middleware/loginstatus.js
@@ -2,18 +2,18 @@ const jwt = require("jsonwebtoken")
 const JWT_SECRET = "thisisquizrowebsitesecret"
 
 
-const loginStatus =(req, res, next)=>{
-    const token = req.header("auth-token");
-    if(!token){
-        res.status(401).send({error:"Session Expired"})
+const loginStatus = (req, res, next) => {
+    const token = req.header("Authorization");
+    if (!token) {
+        res.status(401).send({ error: "Session Expired" })
     }
-    try {        
+    try {
         const decryptToken = jwt.verify(token, JWT_SECRET)
         req.userId = decryptToken;
         next()
     } catch (error) {
         console.log(error)
-        res.status(401).send({error:"Session Expired"})   
+        res.status(401).send({ error: "Session Expired" })
     }
 }
 module.exports = loginStatus;
